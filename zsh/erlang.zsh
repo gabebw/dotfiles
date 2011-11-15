83f3fcad3e6b492e8187549466db3da0b2533f80
Randy Secrist <randy.secrist@gmail.com>
2011-11-15 15:36:30 -0700

    Adjusted erlang path for a second machine.

--- zsh/erlang.zsh
+++ zsh/erlang.zsh
@@ -1,4 +1,10 @@
 ############
 #  Erlang  #
 ############
-. $HOME/bin/erlang-R14B04/activate
+location=$HOME/bin/erlang-R14B04/activate
+
+if [[ -f "$location" ]]; then
+  . $location
+else
+  . $HOME/bin/erlang_r14b04/activate
+fi
