31e1ad7083e31451d622c7518150551bb711f825
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-02-14 15:26:32 -0500

    insert-last-word?!?!?!?! WOWZERS

---
+++ zsh/bindkey.zsh
@@ -0,0 +1,4 @@
+# <Ctrl-N> will insert the last argument passed to the previous command.
+# "It's like !$ but awesomer." - Mike Burns
+# Also, see !* for all of the arguments passed to the previous command.
+bindkey "^N" insert-last-word
