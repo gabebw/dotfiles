e627aac1c092d05e2ebde0f7d6a242ec543f002f
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-02-14 15:38:02 -0500

    Remove some aliases.

--- zsh/rails.zsh
+++ zsh/rails.zsh
@@ -11,15 +11,11 @@ alias guard="be guard"
 alias cuc="be cucumber"
 compdef cuc=cucumber
 
-# Rspec
-alias spec="be rspec"
-
 # Rake
 alias migrate="be rake db:migrate db:test:prepare"
 alias remigrate="be rake db:migrate && be rake db:migrate:redo && be rake db:schema:dump db:test:prepare"
 alias bake="be rake"
 alias brake=bake
-alias take="RAILS_ENV=test be rake"
 alias rrg="bake routes | grep"
 alias reset-db="bake db:drop db:create && migrate"
 
