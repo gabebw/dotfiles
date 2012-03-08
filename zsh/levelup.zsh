2c8229855b7b9bf78f57d634cb5b9eabdb809754
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-03-08 11:20:55 -0500

    more levelup aliases

--- zsh/levelup.zsh
+++ zsh/levelup.zsh
@@ -1,1 +1,9 @@
-alias dump-levelup="pg_restore --verbose --clean --no-acl --no-owner -d levelup_development ~/Downloads/b313.dump"
+email='gab'
+email=$email'ebw'
+email=$email'@'
+email=$email'gmail.com'
+alias add-me-as-a-merchant="./script/runner 'User.find_by_email(%<$email>).tap{|x| x.merchant_id = 133; x.save! }'"
+alias dump-levelup-database="pg_restore --verbose --clean --no-acl --no-owner -d levelup_development ~/Downloads/b313.dump"
+alias aggregate-levelup='bake levelup:{merchant:touch_demographics,loyalty:aggregate,order:aggregate}'
+# Ensure db:drop:all succceeds first, because something else may be accessing it
+alias dump-levelup='bake db:drop:all && bake db:create:all && dump-levelup-database ; migrate && add-me-as-a-merchant'
