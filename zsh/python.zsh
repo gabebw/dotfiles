0568bd3663eb095fbc7d2c5cf1d969dbc8cc677a
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-04-01 14:57:20 -0400

    Remove python config.

--- zsh/python.zsh
+++
@@ -1,8 +0,0 @@
-##########
-# Python #
-##########
-PYTHONSTARTUP=~/.python_startup
-
-# If you install Python packages via pip, binaries will be installed under
-# Python's cellar but not automatically linked into the Homebrew prefix.
-PATH="$(brew --prefix python)"/bin:$PATH
