2dc02a4d395e218568ab84e52d5741f465d0ed1e
Dan Croak <dan@thoughtbot.com>
2012-05-06 20:08:38 -0400

    Add Heroku scripts
    staging:
    
        staging console
        staging migrate
        staging process
        staging open
        watch staging ps
        staging tail
    
    production:
    
        production backup
        production console
        production migrate
        production open
        watch production ps
        production tail
    
    load-backup-into:
    
        load-backup-into staging
        load-backup-into development
    
    Notes on the last command:
    
    * Copy latest production database backup into development database using
      preferred pgbackups + pg_restore method recommended by Heroku.
    * Do not assume local development database name.
    * Get local database name from config/database.yml.
    * This did not previously exist in dotfiles.
    
    Supporting change for these commands:
    
    * Add ~/.bin to the PATH.
    
    https://github.com/thoughtbot/dotfiles/pull/76

--- zsh/rails.zsh
+++ zsh/rails.zsh
@@ -15,19 +15,6 @@ alias tagit='/usr/local/bin/ctags -R --languages=-javascript --langmap="ruby:+.r
 
 alias h=heroku
 
-function staging-rake(){ h run rake "$@" -r staging }
-alias staging='h run console -r staging'
-alias staging-logs='h logs --tail -r staging'
-alias staging-migrate='staging-rake db:migrate'
-
-function production-rake(){ h run rake "$@" -r production }
-alias production='h run console -r production'
-alias production-logs='h logs --tail -r production'
-alias production-migrate='production-rake rake db:migrate'
-
 # Database
 alias db-development-name='egrep "database:.*development" config/database.yml | awk "{print \$2}"'
-alias db-copy-staging-to-local='h pgbackups:capture -r staging && curl `h pgbackups:url -r staging` | db-restore `db-development-name`'
-alias db-copy-production-to-staging='h pgbackups:restore DATABASE `h pgbackups:url -r production` -r staging  --confirm `basename $PWD`-staging'
-alias db-backup-production='h pgbackups:capture -r production'
 alias db-reset="be rake db:drop db:create && migrate"
