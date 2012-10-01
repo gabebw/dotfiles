2225292c218c438b80169ab61d0fe6691a4004ff
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-10-01 14:31:34 -0400

    Update twios alias with new thoughtbot projects.

--- zsh/twios.zsh
+++ zsh/twios.zsh
@@ -1,1 +1,17 @@
-alias twios="~/thoughtbot/This-Week-in-Open-Source/bin/this-week-in-open-source appraisal bourbon bourne capybara-webkit clearance cocaine continuous_sprinkles diesel dotfiles factory_girl factory_girl_rails fake_braintree flutie friendly-identifier high_voltage jester kumade limerick_rake mile_marker pacecar paperclip paperclip-thumbnailer paperclip-watermarker paul_revere shoulda shoulda-context shoulda-matchers sortable_table squirrel suspenders this-week-in-open-source trout what when dotfiles laptop"
+alias twios="~/thoughtbot/This-Week-in-Open-Source/bin/this-week-in-open-source \
+  appraisal ashmont \
+  backbone-support bourbon bourne \
+  capybara-webkit clearance cocaine continuous_sprinkles \
+  diesel dotfiles \
+  factory_girl factory_girl_rails fake_braintree fistface flutie friendly-identifier \
+  guides \
+  high_voltage \
+  jack_up jester \
+  kumade \
+  laptop limerick_rake \
+  mile_marker \
+  neat \
+  pacecar paperclip paul_revere \
+  shoulda shoulda-context shoulda-matchers sortable_table squirrel suspenders \
+  this-week-in-open-source trail-map trout turning \
+  what when"
