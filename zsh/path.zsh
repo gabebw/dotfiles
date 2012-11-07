3e80a96071feebcb9719c8bd4a79145a3fdc3b8f
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-11-07 17:24:05 -0500

    Heroku client

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -23,6 +23,9 @@ MANPATH=/usr/share/man:/usr/local/share/man:/usr/X11/share/man:/usr/X11/man:/usr
 # Homebrew
 PATH="/usr/local/bin:$PATH"
 
+# Heroku standalone client
+PATH="/usr/local/heroku/bin:$PATH"
+
 # Tex
 PATH="$PATH:/usr/local/texlive/2012basic/bin/universal-darwin"
 
