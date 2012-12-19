b50897c709b8958f3cf370939ec5a5d3b51a31f4
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-12-19 10:12:47 -0500

    Add ZSH completions installed by Homebrew

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,7 +1,7 @@
 ################
 #  COMPLETION  #
 ################
-fpath=(~/.dotfiles/zsh/completion-scripts $fpath)
+fpath=(~/.dotfiles/zsh/completion-scripts /usr/local/share/zsh/site-functions $fpath)
 autoload -U compinit && compinit
 
 # complete viw like `which`
