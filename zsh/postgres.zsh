5471aafcf96375fd4a2396e9209d6037eac5a2d5
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-12-19 10:08:34 -0500

    actually take a DB name

--- zsh/postgres.zsh
+++ zsh/postgres.zsh
@@ -2,6 +2,9 @@
 export PSQL_EDITOR="vim -c ':set ft=sql'"
 
 # db-dump DB_NAME
-alias db-dump='pg_dump -Fc -f database.dump && echo Wrote to database.dump'
+function db-dump() {
+  pg_dump -Fc -f database.dump "$1" && echo Wrote to database.dump
+}
+
 # db-restore DB_NAME
 alias db-restore='pg_restore --verbose --clean --no-acl --no-owner -d'
