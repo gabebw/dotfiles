4beea56963a277ba2e5d7b93ebfb30cf77736a49
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-01-12 11:57:46 -0500

    Add a script for installing helpful homebrews

---
+++ helpful-homebrews.zsh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+# Run this script to install some helpful (but not essential) programs.
+
+echo "Installing jq, sed for json..."
+  brew install jq
+
+echo "Installing pgrep, grep for ps..."
+  brew install pgrep
+
+echo "Installing heroku-toolbelt, the recommended way to use Heroku..."
+  brew install heroku-toolbelt
+
+echo "Installing rbenv, a lovely Ruby version manager..."
+  brew install rbenv
+
+echo "Installing ruby-build to get 'rbenv install'..."
+  brew install ruby-build
+
+echo "Installing colordiff to get colorful diffs (alias diff='colordiff -u')..."
+  brew install colordiff
+
+echo "Installing ack, a better grep..."
+  brew install ack
+
+
