04cee530a2ff94aa70ea6b5e7b62ebad88b6a666
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-01-13 11:56:43 -0500

    Rename scripts/ to bin/

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -32,4 +32,4 @@ PATH="$PATH:/usr/local/texlive/2012basic/bin/universal-darwin"
 # Bundler binstubs
 PATH="./bundler_binstubs:$PATH"
 # My scripts are always last. Use full path instead of ~/ so that "which" works.
-PATH=$PATH:"/Users/gabe/bin:/Users/gabe/.dotfiles/scripts"
+PATH=$PATH:"/Users/gabe/bin:/Users/gabe/.dotfiles/bin"
