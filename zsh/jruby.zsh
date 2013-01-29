4d5abc1226364fe625e031efd1b4f82fb1723bb5
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-01-29 09:57:43 -0500

    Give JRuby 2GB

--- zsh/jruby.zsh
+++ zsh/jruby.zsh
@@ -1,6 +1,10 @@
-# Max memory = 1024MB
-JRUBY_OPTS=-J-Xmx1024m
-# Disable JIT
+# Max memory = 2048m
+JRUBY_OPTS=-J-Xmx2048m
+
+# Avoid OutOfMemoryError
+JRUBY_OPTS="$JRUBY_OPTS -J-XX:+CMSClassUnloadingEnabled"
+
+# Disable JIT for faster startup
 JRUBY_OPTS="$JRUBY_OPTS -X-C"
 
 export JRUBY_OPTS
