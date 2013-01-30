8102304f4b0864c2c2447594f9d2a84fc726812b
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-01-30 16:53:07 -0500

    capitalize

--- zsh/jruby.zsh
+++ zsh/jruby.zsh
@@ -1,7 +1,7 @@
 export JRUBY_OPTS="--1.9 \
  -J-Dfile.encoding=UTF-8 \
  -J-Djruby.jit.threshold=10 \
- -J-Djruby.compile.mode=jit \
+ -J-Djruby.compile.mode=JIT \
  -J-Xms1536m \
  -J-Xmx1536m \
  -J-XX:MaxPermSize=512m \
