69bfa4ebe33110e5d72fba067d6c6e888cd952d6
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-01-30 10:55:56 -0500

    use newer jruby options

--- zsh/jruby.zsh
+++ zsh/jruby.zsh
@@ -1,7 +1,7 @@
 export JRUBY_OPTS="--1.9 \
  -J-Dfile.encoding=UTF-8 \
  -J-Djruby.jit.threshold=10 \
- -J-Djruby.compile.mode=FORCE \
+ -J-Djruby.compile.mode=jit \
  -J-Xms1536m \
  -J-Xmx1536m \
  -J-XX:MaxPermSize=512m \
