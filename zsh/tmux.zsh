a1b7a758a7406a933bdd7b6c87b96b949e7b5ef4
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-03-26 10:32:15 -0400

    Tmuxinator

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -1,3 +1,6 @@
 alias copy-from-tmux="tmux saveb - | pbcopy"
 
 alias current-tmux-session="tmux list-sessions | grep attached | awk '{session=sub(/:/, \"\", \$1); print \$session}'"
+
+# gem install tmuxinator
+[[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
