9ada3494689de236f144d0d9f748e6cfb4246651
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-12-12 10:35:29 -0500

    Append history incrementally
    $HISTFILESIZE doesn't do anything, so remove it.

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -3,11 +3,11 @@
 #################
 HISTFILE=~/.zsh_history
 export HISTSIZE=1000000000000000000
-export HISTFILESIZE=$HISTSIZE
 export SAVEHIST=$HISTSIZE
 setopt no_list_beep
 setopt no_beep
-setopt appendhistory
+# Append as you type (incrementally) instead of on shell exit
+setopt inc_append_history
 setopt hist_ignore_all_dups
 setopt hist_reduce_blanks
 setopt autocd
