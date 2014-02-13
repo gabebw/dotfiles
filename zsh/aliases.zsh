62c42d3b285ba285c45a6336565d2941ba798a65
Gabe Berke-Williams <gabe@thoughtbot.com>
2014-02-12 23:09:07 -0500

    Remove unused aliases. Enforce ag.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,14 +11,10 @@ alias du="du -cksh"
 alias df="df -h"
 # Use modern regexps for sed, i.e. "(one|two)", not "\(one\|two\)"
 alias sed="sed -E"
-alias grep=egrep
-alias grpe=grep # fix typo
-export GREP_OPTIONS="--color=auto" # removes color when piping
-export GREP_COLOR='1;31' # highlight matches in red
+grep(){ echo Use ag; }
+alias grpe=grep
 [[ -x $(which colordiff) ]] && alias diff="colordiff -u" || alias diff="diff -u"
-[[ -x $(which colormake) ]] && alias make=colormake
 alias less="less -R" # correctly interpret ASCII color escapes
-alias eject="drutil tray eject"
 
 alias prettyjson="python -m json.tool"
 alias prettyxml="xmllint --format -"
@@ -50,31 +46,4 @@ function p {
 
 icopy() {
   scp "$1" i:~/images
-}
-
-# credit: http://nparikh.org/notes/zshrc.txt
-# Usage: extract <file>
-# Description: extracts archived files / mounts disk images
-# Note: .dmg/hdiutil is Mac OS X-specific.
-extract () {
-    if [ -f $1 ]; then
-        case $1 in
-            *.tar.bz2)  tar -jxvf $1                        ;;
-            *.tar.gz)   tar -zxvf $1                        ;;
-            *.bz2)      bunzip2 $1                          ;;
-            *.dmg)      hdiutil mount $1                    ;;
-            *.gz)       gunzip $1                           ;;
-            *.tar)      tar -xvf $1                         ;;
-            *.tbz2)     tar -jxvf $1                        ;;
-            *.tgz)      tar -zxvf $1                        ;;
-            *.zip)      unzip $1                            ;;
-            *.ZIP)      unzip $1                            ;;
-            *.pax)      cat $1 | pax -r                     ;;
-            *.pax.Z)    uncompress $1 --stdout | pax -r     ;;
-            *.Z)        uncompress $1                       ;;
-            *)          echo "'$1' cannot be extracted/mounted via extract()" ;;
-        esac
-    else
-        echo "'$1' is not a valid file"
-    fi
 }
