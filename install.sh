ac6aa92c35c7820616db25785191ee52a6ac8f19
Gabe Berke-Williams <gabebw@gabebw.com>
2014-03-07 14:54:35 -0500

    Add OSX system settings from @christoomey

--- install.sh
+++ install.sh
@@ -9,3 +9,5 @@ brew bundle
 # Install Vim packages
 git clone git@github.com:gmarik/Vundle.vim.git ./vim/bundle/Vundle.vim
 vim +BundleInstall +qa
+
+echo "If you like what you see in system/osx-settings, run ./system/osx-settings"
