340f40de56ba2376981c4897a3e86ff1b707654d
Gabe Berke-Williams <gabebw@gabebw.com>
2014-03-29 19:31:12 -0400

    Include Tomorrow Night Eighties Terminal theme
    https://github.com/chriskempson/tomorrow-theme

--- install.sh
+++ install.sh
@@ -11,3 +11,4 @@ git clone git@github.com:gmarik/Vundle.vim.git ./vim/bundle/Vundle.vim
 vim +BundleInstall +qa
 
 echo "If you like what you see in system/osx-settings, run ./system/osx-settings"
+echo "If you're using Terminal.app, check out the terminal-themes directory"
