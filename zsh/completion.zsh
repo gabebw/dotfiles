ece61b84dba20719b9fe4a0ecb66b4692dce9d64
Gabe Berke-Williams <gabebw@gabebw.com>
2014-04-20 20:54:44 -0400

    Better ZSH history searching

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,13 @@
 ################
 fpath=(~/.dotfiles/zsh/completion-scripts /usr/local/share/zsh/site-functions $fpath)
 autoload -U compinit && compinit
+
+# https://coderwall.com/p/jpj_6q
+# Search through history for previous commands matching everything up to current
+# cursor position. Move the cursor to the end of line after each match.
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
