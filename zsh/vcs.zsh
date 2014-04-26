167f6e2c3985858c3fd75ac5008116b418de44d7
Gabe Berke-Williams <gabebw@gabebw.com>
2014-04-25 22:15:22 -0400

    Remove brackets and parentheses from $PROMPT

--- zsh/vcs.zsh
+++ zsh/vcs.zsh
@@ -4,17 +4,13 @@
 autoload -Uz vcs_info
 # run before setting $PS1 each time
 function precmd { vcs_info 'prompt'; }
+
 # colors
 RESET_COLOR="%{${reset_color}%}"
 BRANCH_COLOR="%{$fg[red]%}"
-PAREN_COLOR="%{$fg[green]%}"
 
-PAREN_OPEN="${PAREN_COLOR}(${RESET_COLOR}"
-PAREN_CLOSE="${PAREN_COLOR})${RESET_COLOR}"
-
-BASIC_BRANCH="${BRANCH_COLOR}%b${RESET_COLOR}" # e.g. "master"
-# Note leading space!
-PRETTY_BRANCH=" ${PAREN_OPEN}${BASIC_BRANCH}${PAREN_CLOSE}"
+# e.g. "master"
+BRANCH="${BRANCH_COLOR}%b${RESET_COLOR}"
 
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' formats $PRETTY_BRANCH
+zstyle ':vcs_info:*' formats " $BRANCH"
