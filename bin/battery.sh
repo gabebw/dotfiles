9744bf2a78d70544d474223848e74e87c1cfd881
Gabe Berke-Williams <gabebw@gabebw.com>
2014-04-28 17:36:02 -0400

    Consolidate battery script

---
+++ bin/battery.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+battery_percentage="$(pmset -g batt | sed -E '/^.*[[:space:]]([0-9]+)%.*/!d; s//\1/g')"
+battery_time=$(pmset -g batt | grep Internal | awk '{print $4, $5}' | sed 's/;//')
+message="${battery_percentage}% (${battery_time})"
+
+if [[ $battery_percentage > 20 ]]
+then
+  # Print in red if we don't have a lot of battery left
+  echo "#[fg=red]" "$message" "#[fg=default]"
+else
+  echo "${message} "
+fi
