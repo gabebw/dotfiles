4e9d2f1368efcefb68ed9adfeb0250637f2fdcf0
Gabe Berke-Williams <gabebw@gabebw.com>
2014-05-01 21:20:50 -0400

    Uninstall TeX since my resume is HTML now

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -8,15 +8,6 @@ PATH="/usr/local/bin:$PATH"
 # Heroku standalone client
 PATH="/usr/local/heroku/bin:$PATH"
 
-# Tex
-# Add whatever version of Tex we have to $PATH
-# If this worked, I'd do it:
-# PATH="$PATH:/usr/local/texlive/*/bin/universal-darwin"
-for dir in /usr/local/texlive/*
-do
-  PATH="$PATH:${dir}/bin/universal-darwin"
-done
-
 # rbenv
 PATH=~/.rbenv/shims:$PATH
 
