c247a30eff65eff653d2734ae70d56d94c5e7f24
Gabe Berke-Williams <gabebw@gabebw.com>
2014-05-14 22:07:40 -0400

    Show `ls` results well on dark backgrounds
    Thanks to @BlakeWilliams.

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -7,3 +7,8 @@ zmodload -i zsh/complist
 # Colorful lists of possible autocompletions
 # Set to an empty string to use default colors
 zstyle ':completion:*' list-colors ''
+
+# Show ls on dark backgrounds well
+unset LSCOLORS
+export LSCOLORS=gxfxcxdxbxegedabagacad
+export LS_COLORS=gxfxcxdxbxegedabagacad
