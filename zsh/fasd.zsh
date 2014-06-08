b6aba7b7ccf0ddb4f3cb2d4c9227ed2891466d17
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-07 20:35:45 -0400

    Enforce fasd usage

--- zsh/fasd.zsh
+++ zsh/fasd.zsh
@@ -1,10 +1,10 @@
 # Initialize fasd (https://github.com/clvv/fasd)
 eval "$(fasd --init auto)"
 
+function cd {
+  echo "Use j, or v for vim!"
+}
+
 # jump to recently used items
-alias a='fasd -a' # any
-alias s='fasd -s' # show / search / select
-alias d='fasd -d' # directory
-alias f='fasd -f' # file
 alias j='fasd_cd -d' # cd, same functionality as j in autojump
 alias v='f -e vim' # quick opening files with vim
