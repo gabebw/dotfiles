6a40559e637ace5e9687d8f2ebbae8fc78aaa60b
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-07 21:09:45 -0400

    Well that was a bad idea

--- zsh/fasd.zsh
+++ zsh/fasd.zsh
@@ -1,10 +1,6 @@
 # Initialize fasd (https://github.com/clvv/fasd)
 eval "$(fasd --init auto)"
 
-function cd {
-  echo "Use j, or v for vim!"
-}
-
 # jump to recently used items
 alias j='fasd_cd -d' # cd, same functionality as j in autojump
 alias v='f -e vim' # quick opening files with vim
