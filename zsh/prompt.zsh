648d7c1e0ac28cf27b8b165d131cdfe914f09520
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-23 17:12:02 -0400

    Change precmd so it doesn't slow down `git commit`
    I don't think extracting a variable with `$$` (PID) will bite me, but we'll see.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -96,10 +96,15 @@ function _git_dirty {
   fi
 }
 
+
 # precmd is a magic function that's run each time the prompt is shown
 function precmd {
+  # Extracting this to a variable makes it SO much faster. Otherwise `git
+  # commit` took like a second.
+  local git_status_file="/tmp/git-status-$$"
+
   vcs_info 'prompt'
-  $(git status 2> /dev/null >! "/tmp/git-status-$$")
+  $(git status 2> /dev/null >! "$git_status_file")
 }
 
 # OK, now actually set PROMPT and RPROMPT
