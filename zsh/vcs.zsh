37c531b309034f40db7f6a75262286268c6a9d03
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-23 11:53:19 -0400

    Show staged/unstaged changes

--- zsh/vcs.zsh
+++ zsh/vcs.zsh
@@ -3,14 +3,24 @@
 #######################
 autoload -Uz vcs_info
 # run before setting $PS1 each time
-function precmd { vcs_info 'prompt'; }
+function precmd {
+  vcs_info 'prompt'
+  $(git status 2> /dev/null >! "/tmp/git-status-$$")
+}
 
 # colors
 RESET_COLOR="%{${reset_color}%}"
 BRANCH_COLOR="%{$fg_bold[yellow]%}"
+STAGED_UNSTAGED_COLOR="%{$fg_bold[red]%}"
 
-# e.g. "master"
+# %b: branch name, e.g. "master"
 BRANCH="${BRANCH_COLOR}%b${RESET_COLOR}"
+# %u: "U" if there are unstaged changes
+# %c: "S" if there are staged changes
+STAGED_UNSTAGED="${STAGED_UNSTAGED_COLOR}%u%c${RESET_COLOR}"
 
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' formats " $BRANCH"
+# Slower, but required for %u and %c codes in formats
+zstyle ':vcs_info:git*' check-for-changes true
+
+zstyle ':vcs_info:git*' formats " [$BRANCH $STAGED_UNSTAGED]"
