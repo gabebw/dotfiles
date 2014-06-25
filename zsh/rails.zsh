20194692032a5f15b321b9048605956708ac9311
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-24 20:19:36 -0400

    Remove unused functions

--- zsh/rails.zsh
+++ zsh/rails.zsh
@@ -9,9 +9,5 @@ alias migrate="be rake db:migrate db:test:prepare"
 alias remigrate="be rake db:migrate && be rake db:migrate:redo && be rake db:schema:dump db:test:prepare"
 alias rrg="be rake routes | grep"
 
-# Test::Unit
-alias tu="ruby -Itest"
-
 # Database
-alias db-development-name='egrep "database:.*development" config/database.yml | awk "{print \$2}"'
 alias db-reset="be rake db:drop db:create && migrate"
