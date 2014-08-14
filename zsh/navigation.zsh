410c01bbb1df0eb9d8db94f7de9b593a76867a2a
Gabe Berke-Williams <gabebw@gabebw.com>
2014-08-13 23:00:08 -0400

    Fast cd-ing into whetstone

--- zsh/navigation.zsh
+++ zsh/navigation.zsh
@@ -10,7 +10,7 @@ function current-project-path() {
   echo "$TMUX_PROJECT_DIRECTORY/`current-tmux-session`"
 }
 
-cdpath=($HOME/code $HOME/code/* $HOME/code/thoughtbot/*)
+cdpath=($HOME/code $HOME/code/* $HOME/code/whetstone $HOME/code/thoughtbot/*)
 
 function chpwd {
   echo $(pwd) >! "`current-project-path`"
