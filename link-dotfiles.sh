0bd7fe238fb169941b94cab0a79ebe106c2df642
Gabe Berke-Williams <gabebw@gabebw.com>
2014-09-15 17:17:11 -0400

    Use a case statement instead

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -5,11 +5,19 @@ nothing_to_do="true"
 for name in *; do
   target="$HOME/.$name"
   if [ ! -L "$target" ]; then
-    if [ "$name" != "install.sh" -a "$name" != "link-dotfiles.sh" ]; then
-      nothing_to_do=false
-      echo "Creating $target"
-      ln -s "$PWD/$name" "$target"
-    fi
+    case "$name" in
+      install.sh)
+        # Do nothing
+        ;;
+      link-dotfiles.sh)
+        # Do nothing
+        ;;
+      *)
+        nothing_to_do=false
+        echo "Creating $target"
+        ln -s "$PWD/$name" "$target"
+        ;;
+    esac
   fi
 done
 
