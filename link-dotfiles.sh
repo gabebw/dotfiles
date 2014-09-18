9f2962725645d50c06a379a9c756e69e4bc0f347
Gabe Berke-Williams <gabebw@gabebw.com>
2014-09-18 09:09:53 -0400

    Make case statement shorter

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -4,21 +4,19 @@ nothing_to_do="true"
 
 for name in *; do
   target="$HOME/.$name"
+
   if [ ! -L "$target" ]; then
     case "$name" in
-      install.sh)
-        # Do nothing
-        ;;
-      link-dotfiles.sh)
+      install.sh | link-dotfiles.sh)
         # Do nothing
         ;;
       *)
         nothing_to_do=false
-        echo "Creating $target"
+        echo "Linking $target"
         ln -s "$PWD/$name" "$target"
         ;;
     esac
   fi
 done
 
-[ $nothing_to_do == "true" ] && echo "Nothing to do."
+[ "$nothing_to_do" == "true" ] && echo "Nothing to do."
