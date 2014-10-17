cc74921d4caa0c3a63bdde8a710da74ac3b885ac
Gabe Berke-Williams <gabebw@gabebw.com>
2014-10-17 09:07:28 -0400

    Set $VISUAL instead of $EDITOR
    Here's why: http://robots.thoughtbot.com/visual-ize-the-future

--- zsh/editor.zsh
+++ zsh/editor.zsh
@@ -1,17 +1,20 @@
 ############
 #  EDITOR  #
 ############
-export EDITOR="vim -p" # -p = 1 tab/file
-alias vi="$EDITOR"
-alias svi="sudo vim"
+
+# Why set $VISUAL instead of $EDITOR?
+# http://robots.thoughtbot.com/visual-ize-the-future
+export VISUAL="vim -p"
+alias vi="$VISUAL"
+alias svi="sudo $VISUAL"
 
 # Remove vim flags for crontab -e
-alias crontab="EDITOR=vim crontab"
+alias crontab="VISUAL=vim crontab"
 
 function viw {
   local location=$(which "$1")
   if [[ -f "$location" ]]; then
-    vim "$location"
+    "$VISUAL" "$location"
   else
     echo "$location isn't a file."
   fi
