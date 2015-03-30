9634d1002248ba1b6b3cd2fdac6828029d001ab6
Gabe Berke-Williams <gabebw@gabebw.com>
2015-03-30 15:03:06 -0400

    alias summer="spring stop"
    I am the best.

--- zsh/rails.zsh
+++ zsh/rails.zsh
@@ -9,6 +9,8 @@ alias migrate="be rake db:migrate db:test:prepare"
 alias remigrate="be rake db:migrate && be rake db:migrate:redo && be rake db:schema:dump db:test:prepare"
 alias rrg="be rake routes | grep"
 
+alias summer="spring stop"
+
 # Database
 alias db-reset="be rake db:drop db:create && migrate"
 
