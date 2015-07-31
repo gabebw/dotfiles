e607c0940d6495a8d3fa78f8f9a5f6693132f68e
Gabe Berke-Williams <gabebw@gabebw.com>
2015-07-31 16:00:42 -0700

    Easily generate x.cabal file

--- zsh/haskell.zsh
+++ zsh/haskell.zsh
@@ -3,3 +3,10 @@ PATH="$HOME/Library/Haskell/bin:$PATH"
 
 # Just type-check the file. Don't compile it.
 alias hcompile="ghc -fno-code"
+
+function cabal-new() {
+  local name="$1"
+  local file="./$1.cabal"
+  cp ~/.dotfiles/basic-cabal-file "$file"
+  sed -i '' "s/{NAME}/$1/g" "$file"
+}
