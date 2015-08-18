ce1c22ea73baa11a860af72332d83c6ef951acde
Gabe Berke-Williams <gabebw@gabebw.com>
2015-08-17 21:20:31 -0700

    Fuzzy history search with fzf

--- zsh/key_bindings.zsh
+++ zsh/key_bindings.zsh
@@ -1,5 +1,13 @@
 # Vim-style line editing
 bindkey -v
 
-# I want my bck-i-search
-bindkey -M viins "^r" history-incremental-search-backward
+# Fuzzy match against history, edit selected value
+fuzzy-history() {
+  print -z $(fc -l 1 | \
+    tail -2000 | \
+    fzf --tac --reverse --no-sort | \
+    sed 's/ *[0-9]* *//')
+}
+
+# Ctrl-r triggers fuzzy history search
+bindkey -M viins -s '^r' 'fuzzy-history\n'
