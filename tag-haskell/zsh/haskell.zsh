f3fe8dda80dda21fb44906c758246b2bb28d4027
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-10 14:22:44 -0700

    Easily create new Haskell project

--- tag-haskell/zsh/haskell.zsh
+++ tag-haskell/zsh/haskell.zsh
@@ -4,9 +4,32 @@ PATH="$HOME/Library/Haskell/bin:$PATH"
 # Just type-check the file. Don't compile it.
 alias hcompile="ghc -fno-code"
 
-function cabal-new() {
-  local name="$1"
-  local file="./$1.cabal"
-  cp ~/.basic-cabal-file "$file"
-  sed -i '' "s/{NAME}/$1/g" "$file"
+function cabal-init(){
+  cabal init \
+    --non-interactive \
+    --package-name "$1" \
+    --license MIT \
+    --synopsis "This is a synopsis" \
+    --is-executable \
+    --author="Gabe Berke-Williams"
+}
+
+# Start a new haskell project named $1
+function haskell-new(){
+  projectname=$1
+  cabalfile="${projectname}.cabal"
+  print "Cool, creating a project named ${fg_bold[yellow]}${projectname}${reset_color}"
+
+  mkdir "$projectname" &&
+    cd "$projectname" &&
+    cabal sandbox init &&
+    echo ".cabal-sandbox/" >> .gitignore &&
+    echo "cabal.sandbox.config" >> .gitignore &&
+    echo "dist/" >> .gitignore &&
+    cabal-init "$projectname" &&
+    echo 'main = putStrLn "It works!"' > Main.hs &&
+    sed -i '' 's/ +$//g' *cabal &&
+    git init &&
+    cabal run >/dev/null &&
+    print "${fg_bold[green]}Edit Main.hs, run it with \`cabal run\`${reset_color}"
 }
