e4f54aed4aad8b2da393e5c59215083ce0318639
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-26 09:27:12 -0700

    Use GHC.app

--- tag-haskell/zsh/haskell.zsh
+++ tag-haskell/zsh/haskell.zsh
@@ -1,5 +1,10 @@
 PATH="./.cabal-sandbox/bin:$HOME/.cabal/bin:$PATH"
-PATH="$HOME/Library/Haskell/bin:$PATH"
+
+# Add GHC 7.10.2 to the PATH, via https://ghcformacosx.github.io/
+GHC_DOT_APP="/Applications/ghc-7.10.2.app"
+if [ -d "$GHC_DOT_APP" ]; then
+  PATH="$HOME/.local/bin:$GHC_DOT_APP/Contents/bin:$PATH"
+fi
 
 # Just type-check the file. Don't compile it.
 alias hcompile="ghc -fno-code"
