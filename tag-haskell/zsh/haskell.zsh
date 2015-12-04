650be5287aa4d6aa1a328d917ff8ed007b689bea
Gabe Berke-Williams <gabebw@gabebw.com>
2015-12-04 14:49:53 -0800

    Easily create a new Yesod project with Postgres

--- tag-haskell/zsh/haskell.zsh
+++ tag-haskell/zsh/haskell.zsh
@@ -8,3 +8,12 @@ fi
 
 # Just type-check the file. Don't compile it.
 alias hcompile="ghc -fno-code"
+
+new-yesod-project() {
+  name=$1
+  stack new "$1" yesod-postgres
+  cd "$1"
+  stack install yesod-bin cabal-install --install-ghc
+  stack build
+  echo "Run the server: stack exec -- yesod devel"
+}
