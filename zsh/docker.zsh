d59984737b049ab8f78b9fe47fadefd6bcf6e384
Gabe Berke-Williams <gabebw@gabebw.com>
2016-02-05 19:33:17 -0800

    Execute docker-kickstart in my shell's context
    This is so the `eval "$(docker-machine env default)"` runs in the context of the
    current shell.
    
    If it's in script, that script's subshell gets the Docker env then immediately
    exits, which doesn't help me very much.

---
+++ zsh/docker.zsh
@@ -0,0 +1,20 @@
+# This is a function so the `eval` runs in the context of the current shell.
+# If it's in script, that script's subshell gets the Docker env then immediately
+# exits, which doesn't help me very much.
+#
+# https://docs.docker.com/machine/get-started/
+function docker-kickstart(){
+  if ! command -v docker-compose > /dev/null; then
+    brew install docker-compose
+  fi
+
+  if ! command -v docker-machine > /dev/null; then
+    brew install docker-machine
+  fi
+
+  if ! docker-machine ls | grep -Fq default; then
+    docker-machine create --driver virtualbox default
+  fi
+
+  eval "$(docker-machine env default)"
+}
