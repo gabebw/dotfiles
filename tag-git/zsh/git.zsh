c555eac8f6306491e2ca122567f1cae4727c76a7
Gabe Berke-Williams <gabebw@gabebw.com>
2016-03-07 15:59:57 -0800

    Handle new location of diff-highlight

--- tag-git/zsh/git.zsh
+++ tag-git/zsh/git.zsh
@@ -45,3 +45,7 @@ function gcl {
 compdef g=git
 compdef _git gc=git-checkout
 compdef _git ga=git-add
+
+for dir in /usr/local/Cellar/diff-so-fancy/*/third_party/diff-highlight/; do
+  PATH=$PATH:$dir
+done
