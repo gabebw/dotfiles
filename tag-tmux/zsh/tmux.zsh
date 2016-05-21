6e5caf169c671b1a23aed816e280de49578fdc1e
Gabe Berke-Williams <gabebw@gabebw.com>
2016-05-20 19:48:20 -0700

    Extreme powerups for connecting to a tmux session
    `t` by itself opens up a fuzzy-find of project directories, then starts a tmux
    session named after the selected directory. If already in the selected session,
    does nothing.
    
    `t foo` will connect to the tmux session named `foo`, or start a `foo` session
    if it doesn't exist.
    
    `t` also tab-completes to existing sessions.
    
    * Extract `t` to its own script, and move associated functions in as well for
      better encapsulation
    * Create a `projects` script (thanks @christoomey!) that lists all directories
      in `$CDPATH` that have git directories and were modified recently

--- tag-tmux/zsh/tmux.zsh
+++ tag-tmux/zsh/tmux.zsh
@@ -1,6 +1,4 @@
-####################################
-# 1) Ensure we're always in a tmux session
-
+# Ensure we're always in a tmux session
 ensure_we_are_inside_tmux() {
   if _not_in_tmux; then
     _ensure_tmux_is_running
@@ -31,57 +29,4 @@ _ensure_tmux_is_running() {
     # `tmux attach` can use.
     tmux new -d
   fi
-}
-
-# Attach if not in tmux, or switch if we are in tmux
-attach_to_tmux_session() {
-  local session="$1"
-  if _not_in_tmux; then
-    tmux attach -d -t "$1"
-  else
-    tmux switch-client -t "$1"
-  fi
-}
-
-####################################
-# 2) the `t` function
-
-# Use `t blog` to connect to the `blog` session, or create a new session named
-# `blog`.
-#
-# With no arguments, lists all tmux sessions.
-function t {
-  if (( $# == 1 )); then
-    _tmux_try_to_connect_to "$1"
-  else
-    tmux list-sessions -F "#{session_name}"
-  fi
- }
-
-_tmux_session_exists(){
-  tmux list-sessions -F "#{session_name}" | egrep -q "^${1}$"
-}
-
-# Try to connect to a session with the given name.
-# If no such session exists, create it first.
-_tmux_try_to_connect_to() {
-  local session_to_connect_to="${1//./-}"
-
-  if _tmux_session_exists "$session_to_connect_to"; then
-    attach_to_tmux_session "$session_to_connect_to"
-  else
-    _new_tmux_session_named "$session_to_connect_to"
-  fi
-}
-
-# Create a new tmux session with the given name.
-_new_tmux_session_named() {
-  local new_session_name="$1"
-
-  # Create three windows named vim/scratch/server and jump into the first one
-  TMUX= tmux new-session -d -As "$new_session_name" -n vim
-  tmux new-window -t "$new_session_name" -n scratch
-  tmux new-window -t "$new_session_name" -n server
-  tmux select-window -t "$new_session_name" -n
-  tmux attach -t "$session_name"
 }
