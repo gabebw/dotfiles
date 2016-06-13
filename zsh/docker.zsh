911c711b50a3f1b304f3e80ef79eb253f6a1d079
Gabe Berke-Williams <gabebw@gabebw.com>
2016-06-13 16:44:24 -0700

    Only give Virtualbox 1GB of memory

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -13,8 +13,8 @@ function docker-kickstart(){
   fi
 
   if ! docker-machine ls | grep -Fq default; then
-    # Create a machine with 50GB of storage and 4GB of memory
-    docker-machine create --driver virtualbox --virtualbox-disk-size=50000 --virtualbox-memory 4096 default
+    # Create a machine with 50GB of storage and 1GB of memory
+    docker-machine create --driver virtualbox --virtualbox-disk-size=50000 --virtualbox-memory 1024 default
   fi
 
   if [[ "$(docker-machine status default)" != "Running" ]]; then
