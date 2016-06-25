b91cfed27465c291eeed5aabe254eb573fb47187
Gabe Berke-Williams <gabebw@gabebw.com>
2016-06-25 11:20:44 -0700

    `viw` understands aliases, functions, you name it
    Thanks @jsteiner and @christoomey!
    
    * https://github.com/jsteiner/dotfiles-local/commit/2e884a4ea930c7a979ad085cd8babc0335a2a14d
    * https://github.com/christoomey/dotfiles/commit/254f807ab5cf82941e676d1b199144cd4b9b3577

--- zsh/editor.zsh
+++ zsh/editor.zsh
@@ -9,15 +9,3 @@ alias vi="$VISUAL"
 
 # Remove vim flags for crontab -e
 alias crontab="VISUAL=vim crontab"
-
-function viw {
-  local location=$(which "$1")
-  if [[ -f "$location" ]]; then
-    vim "$location"
-  else
-    echo "$location isn't a file."
-  fi
-}
-
-# complete viw like `which`
-compdef viw=which
