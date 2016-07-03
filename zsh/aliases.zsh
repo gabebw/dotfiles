f673aa06a125186534556784434853aff6bb48a3
Gabe Berke-Williams <gabebw@gabebw.com>
2016-07-02 22:46:19 -0700

    curl: Don't _always_ turn on verbose mode
    Setting `--verbose` in the curlrc turns on verbose mode _everywhere_. For
    example, it fills the `brew update` output with HTTP headers. Instead, set an
    alias so that when I type `curl`, it's verbose, but scripts are unaffected.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,7 +11,8 @@ alias df="df -h"
 # Use modern regexps for sed, i.e. "(one|two)", not "\(one\|two\)"
 alias sed="sed -E"
 # Just print request/response headers
-alias curl-debug="curl -vH Fastly-debug:1 -o /dev/null"
+alias curl-debug="command curl -vH Fastly-debug:1 -o /dev/null"
+alias curl="curl --verbose"
 
 # Use modern regexps for grep, and do show color when `grep` is the final
 # command, but don't when piping to something else, because the added color
