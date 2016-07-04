5e4beca14ea50f3044c0baaea6540b8ad3866017
Gabe Berke-Williams <gabebw@gabebw.com>
2016-07-04 16:31:16 -0700

    Make fuzzy history searching sliiightly better
    * Don't limit to the most recent 2000 entries. fzf can handle 20k history
      entries just fine.
    * Use an anchored regexp in sed to show what's being removed (the history
      timestamp)
    * Reformat onto two lines

--- zsh/key_bindings.zsh
+++ zsh/key_bindings.zsh
@@ -2,11 +2,10 @@
 bindkey -v
 
 # Fuzzy match against history, edit selected value
+# For exact match, start the query with a single quote: 'curl
 fuzzy-history() {
-  print -z $(fc -l 1 | \
-    tail -2000 | \
-    fzf --tac --reverse --no-sort | \
-    sed 's/ *[0-9]* *//')
+  selected=$(fc -l 1 | sed 's/^[0-9]* *//' | fzf --tac --reverse --no-sort)
+  print -z "$selected"
 }
 
 # Ctrl-r triggers fuzzy history search
