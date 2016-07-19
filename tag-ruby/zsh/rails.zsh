d507303fff6d593ef25876c8ec43b3bbff77231d
Gabe Berke-Williams <gabebw@gabebw.com>
2016-07-19 15:52:29 -0700

    migrate: Make sure migrations can be rolled back

--- tag-ruby/zsh/rails.zsh
+++ tag-ruby/zsh/rails.zsh
@@ -5,10 +5,10 @@
 alias h=heroku
 
 # Rake
-alias migrate="be rake db:migrate db:test:prepare"
+alias migrate="be rake db:migrate && be rake db:rollback && be rake db:migrate db:test:prepare"
 alias rrg="be rake routes | grep"
 
 # Database
-alias db-reset="be rake db:drop db:create && migrate"
+alias db-reset="be rake db:drop db:create db:migrate db:test:prepare"
 
 alias f=start_foreman_on_unused_port
