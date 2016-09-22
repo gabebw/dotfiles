938c1217838ccb995f96aa9942969f99612d352b
Gabe Berke-Williams <gabebw@gabebw.com>
2016-09-21 18:45:22 -0700

    Don't export CDPATH
    It can really mess up shell scripts, which will do `cd foo` and get
    taken to some random directory in `$CDPATH` rather than the `./foo/`
    that the script author expected.
    
    This article explains it well: https://bosker.wordpress.com/2012/02/12/bash-scripters-beware-of-the-cdpath/

--- zsh/cdpath.zsh
+++ zsh/cdpath.zsh
@@ -1,5 +1,2 @@
-# Use the lowercase version to create as an array, and export the uppercase
-# version. The lowercase version can't be exported, and the uppercase version
-# can't be set to an array.
+# Use lowercase `cdpath` because uppercase `CDPATH` can't be set to an array
 cdpath=($HOME/code $HOME/code/* $HOME/code/thoughtbot/*)
-export CDPATH
