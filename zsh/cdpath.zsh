03d8e205bf0635d411da813a23695662574f6687
Gabe Berke-Williams <gabebw@gabebw.com>
2016-09-21 19:57:07 -0700

    bin/projects no longer depends on exporting CDPATH

--- zsh/cdpath.zsh
+++ zsh/cdpath.zsh
@@ -1,2 +1,7 @@
 # Use lowercase `cdpath` because uppercase `CDPATH` can't be set to an array
 cdpath=($HOME/code $HOME/code/* $HOME/code/thoughtbot/*)
+
+# Exporting $CDPATH is bad:
+# https://bosker.wordpress.com/2012/02/12/bash-scripters-beware-of-the-cdpath/
+# So, export PROJECT_DIRECTORIES instead, but set it to $CDPATH
+export PROJECT_DIRECTORIES=$CDPATH
