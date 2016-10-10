61c4f9d81463bedb3d7052f2c059d2ee411053d5
Gabe Berke-Williams <gabebw@gabebw.com>
2016-10-10 10:35:52 -0700

    Only run Homebrew on OSX

--- install.sh
+++ install.sh
@@ -2,13 +2,23 @@
 
 set -e
 
-echo "Installing Homebrew packages..."
-brew update
-brew tap homebrew/bundle
-brew bundle
-for brewfile in */Brewfile; do
-  brew bundle --file="$brewfile"
-done
+is_osx(){
+  [ "$(uname -s)" = Darwin ]
+}
+
+is_linux(){
+  [ "$(uname -s)" = Linux ]
+}
+
+if is_osx; then
+  echo "Installing Homebrew packages..."
+  brew update
+  brew tap homebrew/bundle
+  brew bundle
+  for brewfile in */Brewfile; do
+    brew bundle --file="$brewfile"
+  done
+fi
 
 echo "Linking dotfiles into ~..."
 # Before `rcup` runs, there is no ~/.rcrc, so we must tell `rcup` where to look.
@@ -19,9 +29,11 @@ RCRC=rcrc rcup -v
 echo "Installing Vim packages..."
 vim +PlugInstall +qa
 
-echo
-echo "If you like what you see in system/osx-settings, run ./system/osx-settings"
-echo "If you're using Terminal.app, check out the terminal-themes directory"
+if is_osx; then
+  echo
+  echo "If you like what you see in system/osx-settings, run ./system/osx-settings"
+  echo "If you're using Terminal.app, check out the terminal-themes directory"
+fi
 
 for setup in tag-*/setup; do
   . "$setup"
