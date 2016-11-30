a948ed8778e8d8dbde169d977044edc2a454fd91
Gabe Berke-Williams <gabebw@gabebw.com>
2016-11-29 21:29:39 -0800

    Assume Postgres.app

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -11,4 +11,7 @@ PATH="/usr/local/heroku/bin:$PATH"
 # Node
 PATH=$PATH:/usr/local/share/npm/bin
 
+# Postgres.app
+PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+
 PATH=$HOME/.bin:$PATH
