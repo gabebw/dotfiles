9ff26553a3f9241b20521ae9eba2c87c2cc43b1d
Gabe Berke-Williams <gabebw@gabebw.com>
2017-03-15 20:07:34 -0700

    Steal some zstyle completions from @mandybess
    https://github.com/mandybess/dotfiles/commit/2cc2b166e9a5672c353927e4547edf433097949f

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -5,6 +5,12 @@ fpath=(~/.zsh/completion-scripts /usr/local/share/zsh/site-functions $fpath)
 autoload -U compinit && compinit
 autoload -U bashcompinit && bashcompinit
 
+# Try to match as-is then match case-insensitively
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# When you paste a <Tab>, don't try to auto-complete
+zstyle ':completion:*' insert-tab pending
+
 # Show a menu (with arrow keys) to select the process to kill.
 # To filter, type `kill vi<TAB>` and it will select only processes whose name
 # contains the substring `vi`.
