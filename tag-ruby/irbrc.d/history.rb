7ea1533cfddcbdf0d153d8296eccc7995a3558a2
Gabe Berke-Williams <gabebw@gabebw.com>
2017-06-22 11:11:32 -0700

    Pry can save its own history

--- tag-ruby/irbrc.d/history.rb
+++ tag-ruby/irbrc.d/history.rb
@@ -1,23 +1,28 @@
-IRB.conf[:USE_READLINE] = true
+# Pry handles history itself
+if defined?(Pry)
+  Pry.config.history.file = "#{ENV["HOME"]}/.irb-save-history"
+else
+  IRB.conf[:USE_READLINE] = true
 
-module Readline
-  module History
-    LOG = "#{ENV["HOME"]}/.irb-save-history"
-    def self.write_log(line)
-      if line != "exit"
-        File.open(LOG, 'ab') {|f| f << "#{line}\n"}
+  module Readline
+    module History
+      LOG = "#{ENV["HOME"]}/.irb-save-history"
+      def self.write_log(line)
+        if line != "exit"
+          File.open(LOG, 'ab') {|f| f << "#{line}\n"}
+        end
+      end
+    end
+
+    alias :old_readline :readline
+    def readline(*args)
+      old_readline(*args).tap do |line|
+        History.write_log(line) rescue nil
       end
     end
   end
 
-  alias :old_readline :readline
-  def readline(*args)
-    old_readline(*args).tap do |line|
-      History.write_log(line) rescue nil
-    end
-  end
+  require 'irb/ext/save-history'
+  IRB.conf[:SAVE_HISTORY] = 1000
+  IRB.conf[:HISTORY_FILE] = Readline::History::LOG
 end
-
-require 'irb/ext/save-history'
-IRB.conf[:SAVE_HISTORY] = 1000
-IRB.conf[:HISTORY_FILE] = "#{ENV["HOME"]}/.irb-save-history"
