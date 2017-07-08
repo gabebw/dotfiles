gabebw/dotfiles

commit 632019cfefc9a3a79aafbf1fc0fd96c9100eaf69

Make `gc` straight-up FANCY
With no arguments, it fuzzy-finds a branch and switches to it. With
arguments, it passes those arguments to `git checkout`.

From superstar @christoomey:
https://github.com/christoomey/dotfiles/blob/250c4115dc4d6a7047763d746ceac41ebfbfcac4/zsh/configs/keybindings.zsh#L40

zshrc
@@ -512,7 +512,16 @@ alias amend-new="git commit --amend"
 alias ga="git add"
 alias gai="git add --interactive"
 alias gcp="git rev-parse HEAD | xargs echo -n | pbcopy"
-alias gc="git checkout"
+gc(){
+  if [[ $# == 0 ]]; then
+    local branch=$(git branch --color=always |\
+      fzf --reverse --ansi --tac |\
+      sed -E 's/^\*?[ \t]*//')
+    git checkout "$branch"
+  else
+    git checkout "$@"
+  fi
+}
 alias gcm="git commit -m"
 
 # Checkout branches starting with my initials

commit 2b75c01d3cf5028861f6db0f78617a18ba53a89e

Fix broken autocomplete for `gc`
It broke in 632019c. This fixes it. No idea why. *shrug*

zshrc
@@ -543,6 +543,7 @@ function gcl {
 }
 
 # Complete `g` like `git`, etc
+alias __git-checkout_main=_git_checkout
 compdef g=git
 compdef _git gc=git-checkout
 compdef _git ga=git-add