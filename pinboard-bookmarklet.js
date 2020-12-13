bd395b483245ddad4f69b93335275cb2e6663438
Gabe Berke-Williams <gabebw@gabebw.com>
2020-12-12 17:45:16 -0800

    Wrap Pinboard bookmarklet in an IIFE

--- pinboard-bookmarklet.js
+++ pinboard-bookmarklet.js
@@ -14,45 +14,54 @@
 // uglified then URI-encoded. To do that, run
 // `bin/pinboard-renegerate-bookmarklet`.
 
-url = location.href;
-withoutQueryParams =
-  location.protocol + "//" + location.hostname + location.pathname;
+// Wrap it in an IIFE so that we can run it >1 time in a row on the same page
+// without running into errors like:
+//   SyntaxError: Can't create duplicate variable: 'hostnameWithoutLeadingWWW'
+// And yes, that one could be fixed just by using `var` instead of `const`, but
+// I'm sure there are other errors just waiting, and an IIFE should fix all of
+// them.
+(function () {
+  let url = location.href;
+  const withoutQueryParams =
+    location.protocol + "//" + location.hostname + location.pathname;
+  let d;
 
-if ("" !== document.getSelection().toString()) {
-  d =
-    "<blockquote>" +
-    document.getSelection().toString().trim() +
-    "</blockquote>";
-} else {
-  d = "";
-}
-
-hostsWithoutQueryParams = [
-  "imdb.com",
-  "theoutline.com",
-  "twitter.com",
-  "tiktok.com",
-  "nytimes.com",
-];
-
-const hostnameWithoutLeadingWWW = location.hostname.replace(/^www./, "");
-if (hostsWithoutQueryParams.indexOf(hostnameWithoutLeadingWWW) !== -1) {
-  url = withoutQueryParams;
-} else if (location.href.match(/youtube.com\/watch/)) {
-  var u = new URL(location);
-  var time = u.searchParams.get("t");
-  url = withoutQueryParams + "?v=" + u.searchParams.get("v");
-  if (time) {
-    url += "&t=" + time;
+  if ("" !== document.getSelection().toString()) {
+    d =
+      "<blockquote>" +
+      document.getSelection().toString().trim() +
+      "</blockquote>";
+  } else {
+    d = "";
   }
-}
-open(
-  "https://pinboard.in/add?url=" +
-    encodeURIComponent(url) +
-    "&description=" +
-    encodeURIComponent(d) +
-    "&title=" +
-    encodeURIComponent(document.title),
-  "Pinboard",
-  "toolbar=no,width=700,height=350"
-);
+
+  const hostsWithoutQueryParams = [
+    "imdb.com",
+    "theoutline.com",
+    "twitter.com",
+    "tiktok.com",
+    "nytimes.com",
+  ];
+
+  var hostnameWithoutLeadingWWW = location.hostname.replace(/^www./, "");
+  if (hostsWithoutQueryParams.indexOf(hostnameWithoutLeadingWWW) !== -1) {
+    url = withoutQueryParams;
+  } else if (location.href.match(/youtube.com\/watch/)) {
+    var u = new URL(location);
+    var time = u.searchParams.get("t");
+    url = withoutQueryParams + "?v=" + u.searchParams.get("v");
+    if (time) {
+      url += "&t=" + time;
+    }
+  }
+  open(
+    "https://pinboard.in/add?url=" +
+      encodeURIComponent(url) +
+      "&description=" +
+      encodeURIComponent(d) +
+      "&title=" +
+      encodeURIComponent(document.title),
+    "Pinboard",
+    "toolbar=no,width=700,height=350"
+  );
+})();
