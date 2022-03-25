593e33fd0d6abade136dc5eb0a3ca529b25d90d3
Gabe Berke-Williams <gabebw@gabebw.com>
2022-03-25 11:57:58 -0700

    Dracula colorscheme

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -208,3 +208,31 @@ eval "$(fnm env --use-on-cd --log-level=error)"
 [ -r ~/.aliases.fish ] && source ~/.aliases.fish
 
 set fish_greeting ""
+
+# Colorscheme: Dracula
+set -U fish_color_normal normal
+set -U fish_color_command F8F8F2
+set -U fish_color_quote F1FA8C
+set -U fish_color_redirection 8BE9FD
+set -U fish_color_end 50FA7B
+set -U fish_color_error FFB86C
+set -U fish_color_param FF79C6
+set -U fish_color_comment 6272A4
+set -U fish_color_match --background=brblue
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion BD93F9
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D
+set -U fish_pager_color_selected_background --background=brblack
