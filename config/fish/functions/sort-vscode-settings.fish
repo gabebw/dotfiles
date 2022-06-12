900c7fe66e4d5febe8e436f8778b709b620f39f4
Gabe Berke-Williams <gabebw@gabebw.com>
2022-06-12 15:11:18 -0700

    sort-vscode-settings: ensure file is valid JSON

--- config/fish/functions/sort-vscode-settings.fish
+++ config/fish/functions/sort-vscode-settings.fish
@@ -1,5 +1,9 @@
 # Run in dotfiles repo
 function sort-vscode-settings
   set -f f "./Library/Application Support/Code/User/settings.json"
-  jq --sort-keys < "$f" | sponge "$f"
+  # Ensure that it's valid JSON before we write to it, because that clears the
+  # file
+  if jq < "$f" > /dev/null
+    jq --sort-keys < "$f" | sponge "$f"
+  end
 end
