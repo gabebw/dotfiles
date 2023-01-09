24074d85a3bd5bc3f2f976046805edc0993ca901
Gabe Berke-Williams <gabebw@gabebw.com>
2023-01-08 18:50:45 -0800

    al: Pass a number, not a directory

--- config/fish/functions/al.fish
+++ config/fish/functions/al.fish
@@ -1,7 +1,11 @@
-function al -a directory
-  if [ -d "$directory" ]
-    lister -n 10 -s modified $directory
-  else
-    lister $argv
+function al -a number
+  if [ $PWD = $HOME ]
+    echo "Not running this in \$HOME."
+    return 1
   end
+
+  # Default to 10 results if no number was set
+  set -q number[1]; or set number 10
+
+  lister -n $number -s modified
 end
