f3efe1c4b6326137e3a999ee1e570966a1e52463
Gabe Berke-Williams <gabebw@gabebw.com>
2023-03-31 12:15:34 -0700

    v: check if variable is set

--- config/fish/functions/v.fish
+++ config/fish/functions/v.fish
@@ -3,7 +3,11 @@ function v -a query
     echo "Refusing to run this in your home directory" >&2
     return 1
   end
-  if [ $TERM_PROGRAM = "vscode" ]
+
+  # Check if $TERM_PROGRAM is set before running tests.
+  # Otherwise it will show an error because it sees `[ = "vscode" ]`, which
+  # isn't valid.
+  if set -q TERM_PROGRAM && [ $TERM_PROGRAM = "vscode" ]
     set -f files (fzf --query=$query --multi --exit-0)
   else
     set -f files (fzf-tmux --query=$query --multi --exit-0)
