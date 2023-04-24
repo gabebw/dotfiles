0680b7682baa1657a7a26e68b9b0701a863950a3
Gabe Berke-Williams <gabebw@gabebw.com>
2023-04-24 10:51:21 -0700

    Prettier

--- bookmarklets/reddit-metric-to-imperial.js
+++ bookmarklets/reddit-metric-to-imperial.js
@@ -1,6 +1,6 @@
-javascript:(function(){
-  const wg = /(\d+(?:\.\d*)?)\s*?(?:(kg|kilo)s?)/ig;
-  const cm = /(\d+(?:\.\d*)?)\s*?cms?/ig;
+javascript: (function () {
+  const wg = /(\d+(?:\.\d*)?)\s*?(?:(kg|kilo)s?)/gi;
+  const cm = /(\d+(?:\.\d*)?)\s*?cms?/gi;
   const conversion_factor = 2.20462262;
 
   function toFeet(cm) {
@@ -19,18 +19,21 @@ javascript:(function(){
   function kgToLb(match, number, unit) {
     const e = parseFloat(number, 10);
     match += ` (${Math.round(e * conversion_factor)}lbs)`;
-    return match
+    return match;
   }
 
   function fixInnerHTML(node) {
     node.innerHTML = node.innerHTML
       .replace(wg, kgToLb)
-      .replace(cm, (match, number) => match + ` (${toFeet(parseFloat(number, 10))})`);
+      .replace(
+        cm,
+        (match, number) => match + ` (${toFeet(parseFloat(number, 10))})`
+      );
   }
 
   /* You can't use line comments because it collapses to a single line */
   /* Post body */
-  document.querySelectorAll('div.md').forEach(fixInnerHTML);
+  document.querySelectorAll("div.md").forEach(fixInnerHTML);
   /* Post title */
-  document.querySelectorAll('p.title a.title').forEach(fixInnerHTML);
-})()
+  document.querySelectorAll("p.title a.title").forEach(fixInnerHTML);
+})();
