7f49603aca0b6a3deafc7a3d3d7d5e6dca3056ac
Gabe Berke-Williams <gabebw@gabebw.com>
2023-10-09 15:15:17 -0700

    Use a prefix for "private" functions

--- config/fish/functions/o.fish
+++ config/fish/functions/o.fish
@@ -1,16 +1,21 @@
+# Use a prefix (`__o_`) because functions are always global, even when defined
+# inside other functions: https://github.com/fish-shell/fish-shell/issues/1799
+
+function __o_custom_fd
+  command fd --no-ignore --type file $argv
+end
+
+function __o_open_images_in_directory -a directory
+  # `--max-depth 1` means "only go 1 level inside the directory and don't recurse"
+  open -a Preview (__o_custom_fd \
+    -e jpg -e png -e jpeg \
+    -a \
+    --base-directory $directory | sort)
+end
+
 # This will recurse infinitely into the directory you give it, so just do `o
 # toplevel/` instead of `o toplevel/**/*.*`.
 function o -a directory
-  function custom_fd; command fd --no-ignore --type file $argv; end
-  function open_images_in_directory
-      # `--max-depth 1` means "only go 1 level inside the directory and don't recurse"
-      open -a Preview (custom_fd \
-        -e jpg -e png -e jpeg \
-        -a \
-        --base-directory $argv[1] | sort)
-
-  end
-
   if set -q directory && [ -d "$directory" ]
     if command fd -e webm -e mp4 -e m4v -e flv -e mov -q --base-directory $directory
       # "-X <command>" means the results are concatenated and the command is
@@ -20,13 +25,13 @@ function o -a directory
         --base-directory $directory \
         -X open
     else
-      open_images_in_directory $directory
+      __o_open_images_in_directory $directory
     end
   else
     if [ (count $argv) -eq 0 ]
       # Are there any images in this directory?
       if [ (count *.{png,jpg}) -gt 0 ]
-        open_images_in_directory .
+        __o_open_images_in_directory .
       else
         o $PWD
       end
