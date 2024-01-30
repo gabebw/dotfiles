aa89a2558616b02ac9da252501bb8a37b2e6005e
Gabe Berke-Williams <gabebw@gabebw.com>
2024-01-30 11:04:45 -0800

    o: just open all files

--- config/fish/functions/o.fish
+++ config/fish/functions/o.fish
@@ -5,36 +5,33 @@ function __o_custom_fd
   command fd --no-ignore --type file $argv
 end
 
+function __o_all_images_in_directory
+  set directory $argv[1]
+  set --erase argv[1]
+  __o_custom_fd -e jpg -e png -e jpeg -a --base-directory $directory $argv
+end
+
 function __o_open_images_in_directory -a directory
   # `--max-depth 1` means "only go 1 level inside the directory and don't recurse"
-  open -a Preview (__o_custom_fd \
-    -e jpg -e png -e jpeg \
-    -a \
-    --base-directory $directory | sort)
+  if __o_all_images_in_directory $directory -q
+    open -a Preview (__o_all_images_in_directory $directory | sort)
+  end
 end
 
 # This will recurse infinitely into the directory you give it, so just do `o
 # toplevel/` instead of `o toplevel/**/*.*`.
 function o -a directory
   if set -q directory && [ -d "$directory" ]
-    if command fd -e webm -e mp4 -e m4v -e flv -e mov -q --base-directory $directory
-      # "-X <command>" means the results are concatenated and the command is
-      # executed once with all found results.
-      __o_custom_fd \
-        -e mp4 -e flv -e mov -e webm -e m4v \
-        --base-directory $directory \
-        -X open
-    else
-      __o_open_images_in_directory $argv
-    end
+    # "-X <command>" means the results are concatenated and the command is
+    # executed once with all found results.
+    __o_custom_fd \
+      -e mp4 -e flv -e mov -e webm -e m4v \
+      --base-directory $directory \
+      -X open
+    __o_open_images_in_directory $directory
   else
     if [ (count $argv) -eq 0 ]
-      # Are there any images in this directory?
-      if [ (count *.{png,jpg}) -gt 0 ]
-        __o_open_images_in_directory .
-      else
-        o $PWD
-      end
+      o $PWD
     else
       open $argv
     end
