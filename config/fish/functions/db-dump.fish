17a9d44f84681840ab523d945e08709b941b14ae
Gabe Berke-Williams <gabebw@gabebw.com>
2024-09-24 19:05:47 -0700

    db-dump also accepts a postgres:// string

--- config/fish/functions/db-dump.fish
+++ config/fish/functions/db-dump.fish
@@ -3,7 +3,7 @@ function db-dump -a db_name filename
     pg_dump --clean --create --format=custom --file=$filename $db_name && \
       echo "Wrote to $filename"
   else
-    echo "Usage: db-dump DB_NAME FILENAME"
+    echo "Usage: db-dump <DB_NAME | CONNECTION_STRING> FILENAME"
     return 1
   end
 end
