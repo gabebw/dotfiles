4d49602c0d1098be3b63f954fa1c51cc57572195
Gabe Berke-Williams <gabebw@gabebw.com>
2024-10-05 12:45:15 -0700

    db-dump: allow passing in filename

--- config/fish/functions/db-dump.fish
+++ config/fish/functions/db-dump.fish
@@ -1,9 +1,10 @@
 function db-dump -a db_name filename
-  if [ (count $argv) -eq 2 ]
-    pg_dump --clean --create --format=custom --file=$filename $db_name && \
-      echo "Wrote to $filename"
+  set -q $filename[1]; or set filename "db.dump"
+  if [ (count $argv) -ge 1 ]
+    pg_dump --clean --create --format=custom --file=$filename $db_name
+    and printf "\n\n\nWrote to $filename\n"
   else
-    echo "Usage: db-dump <DB_NAME | CONNECTION_STRING> FILENAME"
+    echo "Usage: db-dump <DB_NAME | CONNECTION_STRING> [FILENAME = db.dump]"
     return 1
   end
 end
