dc99ed2fa5f82be56dcc847d89b0871ae038e2cd
Gabe Berke-Williams <gabebw@gabebw.com>
2025-04-19 13:06:15 -0700

    Make `word` better
    Why waste time say lot word when few word do trick?

--- config/fish/functions/word.fish
+++ config/fish/functions/word.fish
@@ -1,6 +1,4 @@
 # Get word N from a line
 function word -a n
-  # Assume we're piping in and read STDIN
-  read --local stdin
-  string split ' ' -f $n $stdin
+  string trim | awk '{print $'$n' }'
 end
