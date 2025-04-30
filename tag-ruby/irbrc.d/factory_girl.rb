4b98b3749d3b338524be2cde2ccf620ab98a9e37
Gabe Berke-Williams <gabebw@gabebw.com>
2025-04-30 14:38:23 -0700

    Try factory_bot first

--- tag-ruby/irbrc.d/factory_girl.rb
+++ tag-ruby/irbrc.d/factory_girl.rb
@@ -1,9 +1,9 @@
 def fg_boot
   begin
-    require 'factory_girl_rails'
-    include FactoryGirl::Syntax::Methods
-  rescue LoadError
     require 'factory_bot_rails'
     include FactoryBot::Syntax::Methods
+  rescue LoadError
+    require 'factory_girl_rails'
+    include FactoryGirl::Syntax::Methods
   end
 end
