da5ff040428489353852ebb9eb9bf7424d4bddbf
Gabe Berke-Williams <gabebw@gabebw.com>
2025-05-06 08:47:33 -0700

    Check for pooled Neon connection strings

--- config/fish/functions/db-dump.fish
+++ config/fish/functions/db-dump.fish
@@ -1,7 +1,15 @@
-function db-dump -a db_name filename
-  set -q $filename[1]; or set filename "db.dump"
+function db-dump -a db_name_or_connection_string filename
+  if string match -req 'postgresql://.*pooler.*neon.tech'
+    # Don't `pg_dump` from a pooled connection:
+    # https://github.com/pgbouncer/pgbouncer/issues/452
+    # https://github.com/pgbouncer/pgbouncer/issues/976
+    echo "Do not use pooled connection strings with this. Copy the unpooled version instead" >&2
+    return 1
+  end
+
+  set -q filename; or set filename "db.dump"
   if [ (count $argv) -ge 1 ]
-    pg_dump --clean --create --format=custom --file=$filename $db_name
+    pg_dump --clean --create --format=custom --file=$filename $db_name_or_connection_string
     and printf "\n\n\nWrote to $filename\n"
   else
     echo "Usage: db-dump <DB_NAME | CONNECTION_STRING> [FILENAME = db.dump]"
