7957d3f094d54fbb01cf9fa3bcf913159b9a53c0
Gabe Berke-Williams <gabebw@gabebw.com>
2025-05-07 08:30:08 -0700

    Autoformat CSS

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -477,6 +477,7 @@ require("lazy").setup({
           lua = { "stylua" },
           python = { "ruff" },
           javascript = { "prettierd", "prettier", stop_after_first = true },
+          css = { "prettierd", "prettier", stop_after_first = true },
           ruby = { "standardrb" },
           eruby = { "erb_lint", timeout_ms = 2000 },
           markdown = { "prettierd", "prettier", stop_after_first = true },
