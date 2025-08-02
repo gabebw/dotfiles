40f77ee77108e22e3d4ed2e98d4e4a490dbfce0a
Gabe Berke-Williams <gabebw@gabebw.com>
2025-08-02 10:06:21 -0700

    Set the bundler binstubs path like they recommend

--- config/fish/functions/b.fish
+++ config/fish/functions/b.fish
@@ -1,7 +1,8 @@
 function b --wraps bundle
   if [ (count $argv) -eq 0 ]
     if bundle check > /dev/null || bundle install --jobs=4
-      bundle binstubs --all --path=./bin/stubs
+      bundle config set bin './bin/stubs'
+      bundle binstubs --all
     end
   else
     bundle $argv
