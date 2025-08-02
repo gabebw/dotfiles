ab8d1d69e2c2fab80d0e45a99ba1fafa2d12a345
Gabe Berke-Williams <gabebw@gabebw.com>
2025-08-02 10:10:00 -0700

    Prefer vim-textobj-ruby because it's more precise

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -351,7 +351,18 @@ require("lazy").setup({
     },
     { "vim-ruby/vim-ruby" },
     -- Allow `cir` to change inside ruby block, etc
-    { "nelstrom/vim-textobj-rubyblock", dependencies = { "kana/vim-textobj-user" } },
+    {
+      "rhysd/vim-textobj-ruby",
+      dependencies = { "kana/vim-textobj-user" },
+      init = function()
+        -- definitions blocks	(module, class, def): ro
+        -- loop blocks (while, for, until): rl
+        -- control blocks	(do, begin, if, unless, case): rc
+        -- do statement	(do): rd
+        -- any block (including above): rr
+        vim.g.textobj_ruby_more_mappings = 1
+      end,
+    },
     { "tpope/vim-rake" },
     { "tpope/vim-projectionist" },
     {
