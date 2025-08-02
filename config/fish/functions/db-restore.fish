4a3eb1a0ce0591f6cb5829d2193c2d4870843827
Gabe Berke-Williams <gabebw@gabebw.com>
2025-08-02 10:08:57 -0700

    db-restore: make it work nicely with remote DBs

--- config/fish/functions/db-restore.fish
+++ config/fish/functions/db-restore.fish
@@ -1,15 +1,33 @@
 function db-restore -a db_name filename
+  # Only run >1 job if we're running locally.
+  # Using >1 job on e.g. Neon leads to an error about a "broken pipe".
+  if string match -rq "localhost" $db_name || ! string match -rq 'postgres(ql)?://' $db_name
+    # It's restoring to a local DB
+    set -f is_local true
+  else
+    set -f is_local false
+  end
+
+  if $is_local
+    set -f jobs (getconf _NPROCESSORS_ONLN)
+  else
+    set -f jobs 1
+  end
+
   if [ (count $argv) -eq 2 ]
-    dropdb --if-exists $db_name && \
-    createdb $db_name && \
-      pg_restore \
-        --verbose \
-        --clean \
-        --no-acl \
-        --no-owner \
-        --jobs $(getconf _NPROCESSORS_ONLN) \
-        --dbname $db_name \
-        $filename
+    if $is_local
+      dropdb --if-exists $db_name
+      and createdb $db_name
+    end
+
+    pg_restore \
+      --verbose \
+      --clean \
+      --no-acl \
+      --no-owner \
+      --jobs $jobs \
+      --dbname $db_name \
+      $filename
   else
     echo "Usage: db-restore DB_NAME FILENAME"
     return 1
