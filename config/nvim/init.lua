e0f94cb8d27ea4c76008958f875d5f4e6417a2e7
Gabe Berke-Williams <gabebw@gabebw.com>
2025-08-02 10:16:53 -0700

    Don't turn off netrw
    nvim-tree recommends turning it off, but I need netrw so that I can open
    links with `gx`.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -782,9 +782,6 @@ require("lazy").setup({
         },
       },
       init = function()
-        -- disable netrw at the very start of your init.lua
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
         -- optionally enable 24-bit colour
         vim.opt.termguicolors = true
       end,
