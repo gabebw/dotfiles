b93efea42e87709ad6131d5fac5f8029e6f20e87
Gabe Berke-Williams <gabebw@gabebw.com>
2025-08-02 10:25:16 -0700

    Ignore annoying message from Bundler

--- config/fish/functions/b.fish
+++ config/fish/functions/b.fish
@@ -2,7 +2,7 @@ function b --wraps bundle
   if [ (count $argv) -eq 0 ]
     if bundle check > /dev/null || bundle install --jobs=4
       bundle config set bin './bin/stubs'
-      bundle binstubs --all
+      bundle binstubs --all 2>&1 | rg -v 'Bundler itself does not use binstubs because its version is selected by RubyGems'
     end
   else
     bundle $argv
