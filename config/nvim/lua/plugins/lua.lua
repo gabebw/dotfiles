fffa9255da5cb38ea72d0f2474be30abb5161b46
Gabe Berke-Williams <gabebw@gabebw.com>
2025-08-13 13:11:22 -0700

    deduplicate

--- config/nvim/lua/plugins/lua.lua
+++ config/nvim/lua/plugins/lua.lua
@@ -1,3 +1,14 @@
 return {
   { "wesleimp/stylua.nvim" },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
 }
