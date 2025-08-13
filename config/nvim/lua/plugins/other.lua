d0b0dd10a1adc392ca1c6f98848b9f1e8ea23872
Gabe Berke-Williams <gabebw@gabebw.com>
2025-08-13 13:07:12 -0700

    works

---
+++ config/nvim/lua/plugins/other.lua
@@ -0,0 +1,90 @@
+
+  return  {
+
+    -- Plumbing that makes everything nicer
+    -- Easily comment/uncomment lines in many languages
+    { "tomtom/tcomment_vim" },
+
+    -- <Tab> indents or triggers autocomplete, smartly
+    {
+      "ervandew/supertab",
+      init = function()
+        -- Tell Supertab to start completions at the top of the list, not the bottom.
+        vim.g.SuperTabDefaultCompletionType = "<c-n>"
+      end,
+    },
+    -- Git bindings
+    { "tpope/vim-fugitive" },
+    -- The Hub to vim-fugitive's git
+    { "tpope/vim-rhubarb" },
+    -- Auto-add `end` in Ruby, `endfunction` in Vim, etc
+    { "tpope/vim-endwise" },
+    -- When editing deeply/nested/file, auto-create deeply/nested/ dirs
+    { "duggiefresh/vim-easydir" },
+    -- Cool statusbar
+    { "itchyny/lightline.vim" },
+    -- Easily navigate directories
+    { "tpope/vim-vinegar" },
+    -- Make working with shell scripts nicer ("vim-unix")
+    { "tpope/vim-eunuch" },
+    { "tpope/vim-surround" },
+    -- Make `.` work to repeat plugin actions too
+    { "tpope/vim-repeat" },
+    { "tpope/vim-unimpaired" },
+    -- Intelligently reopen files where you left off
+    { "farmergreg/vim-lastplace" },
+    -- Instead of always copying to the system clipboard, use `cp` (plus motions) to
+    -- copy to the system clipboard. `cP` copies the current line. `cv` pastes.
+    { "christoomey/vim-system-copy" },
+    -- `vim README.md:10` opens README.md at the 10th line, rather than saying "No
+    -- such file: README.md:10"
+    { "xim/file-line" },
+    { "christoomey/vim-sort-motion" },
+    {
+      "zootedb0t/citruszest.nvim",
+      lazy = false,
+      priority = 1000,
+    },
+    {
+      "xolox/vim-easytags",
+      dependencies = { "xolox/vim-misc" },
+      init = function()
+        vim.g.easytags_events = {}
+      end,
+    },
+
+    -- Text objects
+    -- `ae` text object, so `gcae` comments whole file
+    { "kana/vim-textobj-entire", dependencies = { "kana/vim-textobj-user" } },
+    -- `l` text object for the current line excluding leading whitespace
+    { "kana/vim-textobj-line", dependencies = { "kana/vim-textobj-user" } },
+
+    -- Markdown
+    { "tpope/vim-markdown" },
+    { "nicholaides/words-to-avoid.vim", ft = "markdown" },
+    -- It does more, but I'm mainly using this because it gives me markdown-aware
+    -- `gx` so that `gx` works on [Markdown](links).
+    { "christoomey/vim-quicklink", ft = "markdown" },
+    -- Make `gx` work on 'gabebw/dotfiles' too
+    { "gabebw/vim-github-link-opener", branch = "main" },
+
+    -- LSP stuff
+
+    {
+      "pmizio/typescript-tools.nvim",
+      dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+      opts = {},
+    },
+
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
+  }
