bab0a298f186de18f9ebf8f0e54e97a1237d2041
Gabe Berke-Williams <gabebw@gabebw.com>
2025-08-13 13:07:12 -0700

    works

---
+++ config/nvim/lua/config/lazy.lua
@@ -0,0 +1,43 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+ONE_DAY = 60 * 60 * 24
+require("lazy").setup({
+  -- import my plugins from `config/nvim/lua/plugins/*.lua`
+  import = "plugins",
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates every 30 days
+  checker = { enabled = true, frequency = ONE_DAY * 30 },
+  ui = {
+    icons = {
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
+    },
+  },
+})
---
+++ config/nvim/lua/plugins/lua.lua
@@ -0,0 +1,3 @@
+return {
+  { "wesleimp/stylua.nvim" },
+}
