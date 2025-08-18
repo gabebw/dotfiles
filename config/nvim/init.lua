e8db6abaf3a01880c9a67e0f55c82ebd456bbde8
Gabe Berke-Williams <gabebw@gabebw.com>
2025-08-18 11:03:23 -0700

    Turn off mistaken diagnostic

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,6 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+---@diagnostic disable-next-line: undefined-field
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
