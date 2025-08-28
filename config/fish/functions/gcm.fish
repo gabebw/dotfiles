ce69651e58ca45b5bc2c0f0158ba5eaa8b5b5c24
Gabe Berke-Williams <gabebw@gabebw.com>
2025-08-28 15:29:52 -0700

    gcm: support automatically adding a prefix

--- config/fish/functions/gcm.fish
+++ config/fish/functions/gcm.fish
@@ -1,11 +1,17 @@
 # With no arguments: "git checkout [main | master]"
 # With arguments: "git commit" with the arguments as the message
 function gcm
+  set -l COMMIT_PREFIX_FILE ".commit-prefix"
   if [ (count $argv) -eq 0 ]
     git master-to-main-wrapper checkout %BRANCH%
   else
     set -f branch (git current-branch)
     set -f commit_message (string join ' ' $argv)
+    if [ -r (git rev-parse --show-toplevel)/$COMMIT_PREFIX_FILE ]
+      set -l commit_prefix  (cat $COMMIT_PREFIX_FILE | tr -d '\n' | string trim)
+      set commit_message "$commit_prefix $commit_message"
+    end
+
     echo $commit_message
     set -f commit_message_length (string length $commit_message)
     if [ $commit_message_length -gt 50 ]
