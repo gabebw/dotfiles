69fac60e399b30fddb61fc2bc198feb7db25dafa
Gabe Berke-Williams <gabebw@gabebw.com>
2025-09-11 11:42:49 -0700

    Let me do <C-R><C-W> on the command line

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -189,8 +189,7 @@ nnoremap <leader>ev :tabedit $MYVIMRC<CR>
 nnoremap <leader>sv :source $MYVIMRC<CR>
 nnoremap <leader>q :tabedit $HOME/.config/fish/config.fish<CR>
 
-" This is like "<C-R>" in your terminal.
-cmap <C-R> <Plug>(TelescopeFuzzyCommandSearch)
+cnoremap <C-x> <Plug>(TelescopeFuzzyCommandSearch)
 ]]
 
 -- PLUGIN OPTIONS
