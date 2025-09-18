5eb763ab449939c6ed8512c62aa9931fd160e1fe
Gabe Berke-Williams <gabebw@gabebw.com>
2025-09-18 11:19:39 -0700

    Correctly find .commit-prefix file

--- config/fish/functions/gcm.fish
+++ config/fish/functions/gcm.fish
@@ -1,14 +1,16 @@
 # With no arguments: "git checkout [main | master]"
 # With arguments: "git commit" with the arguments as the message
 function gcm
-  set -l COMMIT_PREFIX_FILE ".commit-prefix"
+  set -l COMMIT_PREFIX_FILENAME ".commit-prefix"
+  set -l COMMIT_PREFIX_FILE_PATH (git rev-parse --show-toplevel)/$COMMIT_PREFIX_FILENAME
+
   if [ (count $argv) -eq 0 ]
     git master-to-main-wrapper checkout %BRANCH%
   else
     set -f branch (git current-branch)
     set -f commit_message (string join ' ' $argv)
-    if [ -r (git rev-parse --show-toplevel)/$COMMIT_PREFIX_FILE ]
-      set -l commit_prefix  (cat $COMMIT_PREFIX_FILE | tr -d '\n' | string trim)
+    if [ -r $COMMIT_PREFIX_FILE_PATH ]
+      set -l commit_prefix  (cat $COMMIT_PREFIX_FILE_PATH | tr -d '\n' | string trim)
       set commit_message "$commit_prefix $commit_message"
     end
 
