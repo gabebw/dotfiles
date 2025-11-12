55a41ad1cec0ecf810e403e11ac90cfb46bb53d9
Gabe Berke-Williams <gabebw@gabebw.com>
2025-11-12 15:08:41 -0800

    Prefer flash to leap

--- config/nvim/lua/plugins/leap.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "ggandor/leap.nvim",
-    init = function()
-      vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
-      vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
-    end,
-  },
-}
