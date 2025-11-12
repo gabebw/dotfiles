574908ab7b147b9b628c10f1e17c05aaca4a54fe
Gabe Berke-Williams <gabebw@gabebw.com>
2025-11-12 15:11:27 -0800

    Vim: make which-key more available

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -6,9 +6,19 @@ return {
     -- https://github.com/folke/which-key.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
     preset = "modern",
     triggers = {
-      { "<auto>", mode = "nxso" },
+      { "<auto>", mode = "nxsoc" },
       { "q", mode = { "n" } },
     },
+    ---@type wk.Win.opts
+    win = {
+      -- 0.5 = 50%
+      -- 50 = 50 lines
+      height = { min = 0.5, max = 0.8 },
+      -- I don't care if it hides the cursor
+      no_overlap = false,
+    },
+    -- expand groups if <=N mappings, e.g. show `g=` and `gc` instead of hiding under `g`
+    expand = 5,
   },
   keys = {
     {
@@ -17,6 +27,14 @@ return {
         require("which-key").show({ global = false })
       end,
       desc = "Buffer Local Keymaps (which-key)",
+    },
+    {
+      "<c-k>",
+      mode = { "c" },
+      function()
+        require("which-key").show({})
+      end,
+      desc = "Command Mode Keymaps (which-key)",
     },
   },
 }
