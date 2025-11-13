847aaa3a5117d733f50c4fe728e48ba9ad3cf083
Gabe Berke-Williams <gabebw@gabebw.com>
2025-11-12 21:02:16 -0800

    Vim: set up tree sitter

---
+++ config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+  ---@type TSConfig
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { "typescript" },
+    -- It does nothing unless this is `true`
+    highlight = { enable = true },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
