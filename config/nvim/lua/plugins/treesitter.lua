c73bed64463b9079342605aed34b847b7baa52f7
Gabe Berke-Williams <gabebw@gabebw.com>
2025-11-13 09:35:13 -0800

    Install treesitter for tsx (typescriptreact)

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
   ---@diagnostic disable-next-line: missing-fields
   opts = {
     -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-    ensure_installed = { "typescript" },
+    ensure_installed = { "typescript", "tsx" },
     -- It does nothing unless this is `true`
     highlight = { enable = true },
   },
