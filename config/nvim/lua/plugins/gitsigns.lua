b3dd8460609ad406ec99c044a2e8e0bcaf22d51d
Gabe Berke-Williams <gabebw@gabebw.com>
2025-11-22 11:11:53 -0800

    Make gitsigns fancier

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,39 @@
 return {
-  { "lewis6991/gitsigns.nvim" },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      -- On hover, show blame for current line
+      current_line_blame = true,
+
+      on_attach = function(bufnr)
+        local gitsigns = require "gitsigns"
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]h", bang = true })
+          else
+            gitsigns.nav_hunk "next"
+          end
+        end, { desc = "Next Git hunk" })
+
+        map("n", "[h", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[h", bang = true })
+          else
+            gitsigns.nav_hunk "prev"
+          end
+        end, { desc = "Previous Git hunk" })
+
+        -- Text object
+        map({ "o", "x" }, "ih", gitsigns.select_hunk, { desc = "Git hunk" })
+      end,
+    },
+  },
 }
