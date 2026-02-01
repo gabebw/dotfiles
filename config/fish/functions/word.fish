c8264d46a0df9b5928d899b68c1d687b9d68bf4b
Gabe Berke-Williams <gabebw@gabebw.com>
2026-02-01 14:55:31 -0800

    Read in piped input

--- config/fish/functions/word.fish
+++ config/fish/functions/word.fish
@@ -1,4 +1,6 @@
 # Get word N from a line
 function word -a n
-  string trim | awk '{print $'$n' }'
+  # Why `cat 1>|`?
+  # https://github.com/fish-shell/fish-shell/issues/206#issuecomment-255232968
+  cat 1>| string trim | awk '{print $'$n' }'
 end
