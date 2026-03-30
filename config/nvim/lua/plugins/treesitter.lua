5be555c70722ffc9e3beddb9ad92b3a5d0e2cec8
Gabe Berke-Williams <gabebw@gabebw.com>
2026-03-30 09:02:36 -0700

    Use main branch of nvim-treesitter for nvim 0.12

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,15 +3,13 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
     lazy = false,
     build = ":TSUpdate",
-    ---@module "nvim-treesitter"
+    ---@module "nvim-treesitter.config"
     ---@type TSConfig
-    ---@diagnostic disable-next-line: missing-fields
-    opts = {
-      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-      ensure_installed = {
+    opts = {},
+    setup = function()
+      require("nvim-treesitter").install({
         "css",
         "embedded_template", -- now it understands Erb
         "fish",
@@ -22,13 +20,7 @@ return {
         "sql",
         "tsx",
         "typescript",
-      },
-      -- It does nothing unless this is `true`
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+      })
     end,
   },
 }
