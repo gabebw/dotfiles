e0ed1f4afe68d751a97a9d4dd405e32833e6f7fd
Gabe Berke-Williams <gabebw@gabebw.com>
2026-06-07 13:38:42 -0700

    Automatically enable tree-sitter
    Finally I figured out the right incantations for this.

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -6,10 +6,7 @@ return {
     branch = "main",
     lazy = false,
     build = ":TSUpdate",
-    ---@module "nvim-treesitter.config"
-    ---@type TSConfig
-    opts = {},
-    setup = function()
+    config = function()
       require("nvim-treesitter").install({
         "css",
         "embedded_template", -- now it understands Erb
@@ -21,6 +18,22 @@ return {
         "sql",
         "tsx",
         "typescript",
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local parser = vim.treesitter.get_parser()
+
+          if parser then
+            -- Use treesitter language because it doesn't always match the filetype: for `*.erb`
+            -- files, the filetype is "erb" but the treesitter language is "embedded_template"
+            local treesitter_language = parser:lang()
+            local parsers = require("nvim-treesitter").get_installed()
+            if vim.tbl_contains(parsers, treesitter_language) then
+              pcall(vim.treesitter.start, args.buf)
+            end
+          end
+        end,
       })
     end,
   },
