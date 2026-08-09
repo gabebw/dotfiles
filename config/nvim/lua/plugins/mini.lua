4d2e7f10cce1aaba5e37c87bdbed3b26359c879a
Gabe Berke-Williams <gabebw@gabebw.com>
2026-08-09 11:42:04 -0700

    Save session once, not per-file

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -13,21 +13,19 @@ return {
       require("mini.comment").setup()
     end,
     init = function()
-      -- Only create the autocmd to save the session when an actual file is opened (i.e. not an
-      -- unnamed scratch buffer that I close without saving).
-      -- Sessions are scoped to each git repo by using `Sessions.name`.
-      vim.api.nvim_create_autocmd("BufReadPre", {
+      vim.api.nvim_create_autocmd("VimLeavePre", {
         callback = function()
-          vim.api.nvim_create_autocmd("VimLeavePre", {
-            callback = function()
-              local name = Sessions.name()
-              if name then
-                MiniSessions.write(name)
-              end
-            end,
-            pattern = "*",
-          })
+          -- Only save the session when an actual file is opened (i.e. not an
+          -- unnamed scratch buffer that I close without saving).
+          local is_unnamed_scratch_buffer = #vim.fn.expand "%" == 0
+          -- I hit Ctrl-v while editing my command line to open it in Vim
+          local is_command_line = vim.fn.expand "%:t" == "command-line.fish"
+          if is_unnamed_scratch_buffer or is_command_line then
+            return
+          end
+          MiniSessions.write "Session.vim"
         end,
+        pattern = "*",
       })
     end,
   },
